e70c51d2a4a39e390cf44153ccf7b7e8b43d3d34
Timothy jan <jan.timothy@gmail.com>
2023-11-27 16:51:45 -0800

    Name change to Jantek-SQL-Web-Report-V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Jantek-SQL-Login';
+  title = 'Jantek-SQL-Web-Report-V1';
 }
